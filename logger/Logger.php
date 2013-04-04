52eaf4562d481e8c3e87ed6e1feb4cb87ee8064c
Andrew Martin <andrew.martin@ncl.ac.uk>
2013-04-04 13:35:10 +0100

    *

--- logger/Logger.php
+++ logger/Logger.php
@@ -1,30 +1,30 @@
 <?php
     /*
      * This file is part of Panopto-PHP-Client.
-     * 
+     *
      * Panopto-PHP-Client is free software: you can redistribute it and/or modify
      * it under the terms of the GNU General Public License as published by
      * the Free Software Foundation, either version 3 of the License, or
      * (at your option) any later version.
-     * 
+     *
      * Panopto-PHP-Client is distributed in the hope that it will be useful,
      * but WITHOUT ANY WARRANTY; without even the implied warranty of
      * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
      * GNU General Public License for more details.
-     * 
+     *
      * You should have received a copy of the GNU General Public License
      * along with Panopto-PHP-Client.  If not, see <http://www.gnu.org/licenses/>.
-     * 
+     *
      * Copyright: Andrew Martin, Newcastle University
-     * 
+     *
      */
 
 class Logger
 {
-    private $seed; //Seed each call to the log instance with something, this allows you to differentiate logs calls
+    private $seed; //Seed each call to the log instance with something, this allows you to differentiate logs calls by user for example
     private $logFile; //The name of the file on the server
     private $fh; //File Handle
-    
+
     public function __construct($logFile,$seed = "")
     {
         $this->seed = $seed!=""?" ".$seed:"";
@@ -41,7 +41,7 @@ class Logger
     {
         $this->log(var_export($object,true));
     }
-    
+
     public function log($str)
     {
        fwrite($this->fh,date("Y-m-d H:i:s").$this->seed." : ".$str."\n");
